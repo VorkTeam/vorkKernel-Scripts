f48a08a0bfc4d08a841670f3d365708c9b391725
vork <boss.benedikt@googlemail.com>
2011-05-21 13:06:15 +0200

    not needed anymore

--- CMScripts/release.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-NOW=$(date +"%d%m%y")
-sed -i 's/TESTVR/'$NOW'/g' /home/$USER/CMKernelLG/lge-kernel-star/arch/arm/configs/vorkKernel*
-export release=test
--- CMScripts/test.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-sed -i 's/-vorkKernel-.*/-vorkKernel-TESTVR"/g' /home/$USER/CMKernelLG/lge-kernel-star/arch/arm/configs/vorkKernel*
-export release=test
