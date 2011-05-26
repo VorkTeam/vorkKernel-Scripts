8673f86cbc113ee8028223f9758ec122cae72891
Benedikt Boss <vork.android@gmail.com>
2011-05-26 16:31:04 -0400

    Awesome.zip use boot.img

--- CMScripts/Awesome.zip/tmp/vorkKernel/installkernel.sh
+++ CMScripts/Awesome.zip/tmp/vorkKernel/installkernel.sh
@@ -56,15 +56,9 @@ fi
 ui_print "Packing kernel..."
 cd $basedir
 if [ "$hdrec" == "1" ]; then
-mv 1080p/zImage zImage
+mv 1080p/boot.img boot.img
 mv 1080p/media_profiles.xml media_profiles.xml
-cline="mem=383M@0M nvmem=128M@384M loglevel=0 muic_state=1 lpj=9994240 CRC=3010002a8e458d7 vmalloc=256M brdrev=1.0 video=tegrafb console=ttyS0,115200n8 usbcore.old_scheme_first=1 tegraboot=sdmmc tegrapart=recovery:35e00:2800:800,linux:34700:1000:800,mbr:400:200:800,system:600:2bc00:800,cache:2c200:8000:800,misc:34200:400:800,userdata:38700:c0000:800 androidboot.hardware=p990"
-	else
-cline="mem=447M@0M nvmem=64M@447M loglevel=0 muic_state=1 lpj=9994240 CRC=3010002a8e458d7 vmalloc=256M brdrev=1.0 video=tegrafb console=ttyS0,115200n8 usbcore.old_scheme_first=1 tegraboot=sdmmc tegrapart=recovery:35e00:2800:800,linux:34700:1000:800,mbr:400:200:800,system:600:2bc00:800,cache:2c200:8000:800,misc:34200:400:800,userdata:38700:c0000:800 androidboot.hardware=p990"
 fi
-
-mkbootimg --kernel zImage --ramdisk ramdisk-boot --cmdline "$cline" -o newBoot.img --base 0x10000000
 
 cd /system/etc
 cp $basedir/media_profiles.xml media_profiles.xml
-
