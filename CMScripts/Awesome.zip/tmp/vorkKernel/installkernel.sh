6b0169a76c99e62e7a57bb85996a33100fc79de2
vork <boss.benedikt@googlemail.com>
2011-05-27 05:27:55 +0200

    baconcooker version + boot.img merging on the device

--- CMScripts/Awesome.zip/tmp/vorkKernel/installkernel.sh
+++ CMScripts/Awesome.zip/tmp/vorkKernel/installkernel.sh
@@ -30,8 +30,8 @@ updatename=`echo $UPDATE_FILE | $awk '{ sub(/^.*\//,"",$0); sub(/.zip$/,"",$0); 
 args=`echo $updatename | $awk 'BEGIN {RS="-"}; NR>2 {print}'`
 
 ui_print ""
-ui_print " Installing $kernelver"
-ui_print " Developed by Benee and kiljacken"
+ui_print "Installing $kernelver"
+ui_print "Developed by Benee and kiljacken"
 ui_print ""
 ui_print "Parsing parameters..."
 flags=
@@ -39,6 +39,9 @@ for pp in $args; do
   if [ "$pp" == "1080p" ]; then
       hdrec=1
       flags="$flags -1080p"
+  elif [ "$pp" == "BC" ]; then
+      baconcooker=1
+      flags="$flags -baconcooker"
   else
       errors=$((errors + 1))
       ui_print "ERROR: unknown argument -$pp"
@@ -56,9 +59,23 @@ fi
 ui_print "Packing kernel..."
 cd $basedir
 if [ "$hdrec" == "1" ]; then
-mv 1080p/boot.img boot.img
-mv 1080p/media_profiles.xml media_profiles.xml
+	if [ "$baconcooker" == "1" ]; then 
+		mv Images/1080p/zImageBC zImage
+	else
+		mv Images/1080p/zImage zImage
+	fi
+cline="mem=447M@0M nvmem=64M@447M loglevel=0 muic_state=1 lpj=9994240 CRC=3010002a8e458d7 vmalloc=256M brdrev=1.0 video=tegrafb console=ttyS0,115200n8 usbcore.old_scheme_first=1 tegraboot=sdmmc tegrapart=recovery:35e00:2800:800,linux:34700:1000:800,mbr:400:200:800,system:600:2bc00:800,cache:2c200:8000:800,misc:34200:400:800,userdata:38700:c0000:800 androidboot.hardware=p990"
+mv Images/1080p/media_profiles.xml media_profiles.xml
+else
+	if [ "$baconcooker" == "1" ]; then 
+		mv Images/zImageBC zImage
+	else
+		mv Images/zImage zImage
+	fi
+cline="mem=383M@0M nvmem=128M@384M loglevel=0 muic_state=1 lpj=9994240 CRC=3010002a8e458d7 vmalloc=256M brdrev=1.0 video=tegrafb console=ttyS0,115200n8 usbcore.old_scheme_first=1 tegraboot=sdmmc tegrapart=recovery:35e00:2800:800,linux:34700:1000:800,mbr:400:200:800,system:600:2bc00:800,cache:2c200:8000:800,misc:34200:400:800,userdata:38700:c0000:800 androidboot.hardware=p990"
 fi
+
+/tmp/vorkKernel/mkbootimg --kernel /tmp/vorkKernel/zImage --ramdisk /tmp/vorkKernel/ramdisk-boot --cmdline "$cline" -o /tmp/vorkKernel/newBoot.img --base 0x10000000
 
 cd /system/etc
 cp $basedir/media_profiles.xml media_profiles.xml
--- CMScripts/Scripts/move_ramdisk.sh
+++ CMScripts/Scripts/move_ramdisk.sh
@@ -3,14 +3,6 @@
 if [ -d $SCRIPT_DIR/CMScripts/Tools/boot.img-ramdisk ]
   then
     echo Found boot.img-ramdisk
-
-    if [ -e $SCRIPT_DIR/CMScripts/Tools/zImage ]
-    then
-      echo Found zImage
-    else
-      echo Did not find $SCRIPT_DIR/CMScripts/Tools/zImage
-      exit 0
-    fi
   else
     echo Did not find $SCRIPT_DIR/CMScripts/Tools/boot.img-ramdisk folder!
     exit 0
@@ -20,6 +12,6 @@ cd $SCRIPT_DIR/CMScripts/Tools
 
 ./mkbootfs boot.img-ramdisk | gzip > ramdisk-boot
 
-./mkbootimg --kernel zImage --ramdisk ramdisk-boot --cmdline "$cline" -o newBoot.img --base 0x10000000
+cp ramdisk-boot $SCRIPT_DIR/CMScripts/Awesome.zip/tmp/vorkKernel/ramdisk-boot
 
 cd $SCRIPT_DIR/CMScripts/
