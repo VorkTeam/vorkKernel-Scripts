166c1f9479a47efc905062582da54353a6bb8dd8
Benedikt Boss <vork.android@gmail.com>
2011-05-30 03:09:00 -0400

    douchness fix ... again

--- CMScripts/Awesome.zip/tmp/vorkKernel/installkernel.sh
+++ CMScripts/Awesome.zip/tmp/vorkKernel/installkernel.sh
@@ -75,7 +75,7 @@ else
 cline="mem=383M@0M nvmem=128M@384M loglevel=0 muic_state=1 lpj=9994240 CRC=3010002a8e458d7 vmalloc=256M brdrev=1.0 video=tegrafb console=ttyS0,115200n8 usbcore.old_scheme_first=1 tegraboot=sdmmc tegrapart=recovery:35e00:2800:800,linux:34700:1000:800,mbr:400:200:800,system:600:2bc00:800,cache:2c200:8000:800,misc:34200:400:800,userdata:38700:c0000:800 androidboot.hardware=p990"
 fi
 
-/tmp/vorkKernel/mkbootimg --kernel /tmp/vorkKernel/zImage --ramdisk /tmp/vorkKernel/ramdisk-boot --cmdline "$cline" -o /tmp/vorkKernel/newBoot.img --base 0x10000000
+/tmp/vorkKernel/mkbootimg --kernel /tmp/vorkKernel/zImage --ramdisk /tmp/vorkKernel/ramdisk-boot --cmdline "$cline" -o /tmp/vorkKernel/boot.img --base 0x10000000
 
 cd /system/etc
 cp $basedir/media_profiles.xml media_profiles.xml
