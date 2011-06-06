11343249ac0a143e343c4f5488a8ae103d92b3fe
Benedikt Boss <vork.android@gmail.com>
2011-06-06 05:05:16 -0400

    moar basedir!

--- CMScripts/Awesome.zip/tmp/vorkKernel/installkernel.sh
+++ CMScripts/Awesome.zip/tmp/vorkKernel/installkernel.sh
@@ -81,15 +81,15 @@ ui_print "Packing kernel..."
 cd $basedir
 if [ "$hdrec" == "1" ]; then
 	if [ "$baconcooker" == "1" ]; then 
-		cp Images/1080p/zImageBC zImage
+		cp $basedir/Images/1080p/zImageBC $basedir/zImage
 	else
-		cp Images/1080p/zImage zImage
+		cp $basedir/Images/1080p/zImage $basedir/zImage
 	fi
 	cline="mem=383M@0M nvmem=128M@384M loglevel=0 muic_state=1 lpj=9994240 CRC=3010002a8e458d7 vmalloc=256M brdrev=1.0 video=tegrafb console=ttyS0,115200n8 usbcore.old_scheme_first=1 tegraboot=sdmmc tegrapart=recovery:35e00:2800:800,linux:34700:1000:800,mbr:400:200:800,system:600:2bc00:800,cache:2c200:8000:800,misc:34200:400:800,userdata:38700:c0000:800 androidboot.hardware=p990"
 
 	if [ "$leCam" == "1" ]; then
 	  rm /system/app/Camera.apk
-	  cp files/Camera.apk /system/app/Camera.apk
+	  cp $basedir/files/Camera.apk /system/app/Camera.apk
 	  chmod 644 /system/app/Camera.apk
 	  rm /system/etc/media_profiles.xml
 	  cp $basedir/files/media_profiles.xml-le1080 /system/etc/media_profiles.xml
@@ -108,15 +108,15 @@ if [ "$hdrec" == "1" ]; then
 
 else
 	if [ "$baconcooker" == "1" ]; then 
-		cp Images/zImageBC zImage
+		cp $basedir/Images/zImageBC $basedir/zImage
 	else
-		cp Images/zImage zImage
+		cp $basedir/Images/zImage $basedir/zImage
 	fi
 	cline="mem=447M@0M nvmem=64M@447M loglevel=0 muic_state=1 lpj=9994240 CRC=3010002a8e458d7 vmalloc=256M brdrev=1.0 video=tegrafb console=ttyS0,115200n8 usbcore.old_scheme_first=1 tegraboot=sdmmc tegrapart=recovery:35e00:2800:800,linux:34700:1000:800,mbr:400:200:800,system:600:2bc00:800,cache:2c200:8000:800,misc:34200:400:800,userdata:38700:c0000:800 androidboot.hardware=p990"
 
 	if [ "$leCam" == "1" ]; then
 	  rm /system/app/Camera.apk
-	  cp files/Camera.apk /system/app/Camera.apk
+	  cp $basedir/files/Camera.apk /system/app/Camera.apk
 	  chmod 644 /system/app/Camera.apk
 	  rm /system/etc/media_profiles.xml
 	  cp $basedir/files/media_profiles.xml-le720 /system/etc/media_profiles.xml
@@ -136,7 +136,7 @@ fi
 
 if [ "ril405" == "1" ]; then
 	rm /system/lib/lge-ril.so
-	cp files/ril/405/lge-ril.so /system/lib/lge-ril.so
+	cp $basedir/files/ril/405/lge-ril.so /system/lib/lge-ril.so
         if [ "$?" -ne 0 -o ! -f /system/lib/lge-ril.so ]; then
           ui_print "WARNING: Copying 405 RIL failed!"
           warning=$((warning + 1))
@@ -145,7 +145,7 @@ fi
 
 if [ "ril502" == "1" ]; then
 	rm /system/lib/lge-ril.so
-	cp files/ril/502/lge-ril.so /system/lib/lge-ril.so
+	cp $basedir/files/ril/502/lge-ril.so /system/lib/lge-ril.so
         if [ "$?" -ne 0 -o ! -f /system/lib/lge-ril.so ]; then
           ui_print "WARNING: Copying 502 RIL failed!"
           warning=$((warning + 1))
@@ -154,9 +154,9 @@ fi
 
 if [ "internal" == "1" ]; then
 	rm /system/etc/vold.fstab
-	cp files/vold.fstab /system/etc/vold.fstab
+	cp $basedir/files/vold.fstab /system/etc/vold.fstab
 	chmod 644 /system/etc/vold.fstab
-	cp files/90mountExt /system/etc/init.d/90mountExt
+	cp $basedir/files/90mountExt /system/etc/init.d/90mountExt
 	chmod 750 /system/etc/init.d/90mountExt
         if [ "$?" -ne 0 -o ! -f /system/etc/vold.fstab ! -f /system/etc/init.d/90mountExt ]; then
           ui_print "WARNING: Changing default storage failed!"
