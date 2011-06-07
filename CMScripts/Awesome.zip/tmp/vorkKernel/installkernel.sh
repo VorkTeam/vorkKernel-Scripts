f1a171c948e5d1d6650b03f014f30efeac6d008e
Benedikt Boss <boss.benedikt@googlemail.com>
2011-06-07 01:32:06 -0700

    fix super douchy typo (Thanks to Cpasjuste for pointing it out)

--- CMScripts/Awesome.zip/tmp/vorkKernel/installkernel.sh
+++ CMScripts/Awesome.zip/tmp/vorkKernel/installkernel.sh
@@ -94,7 +94,7 @@ else
 	else
 		cp $basedir/Images/zImage $basedir/zImage
 	fi
-	cline="mem=447M@0M nvmem=64M@447M loglevel=0 muic_state=1 lpj=9994240 CRC=3010002a8e458d7 vmalloc=256M brdrev=1.0 video=tegrafb console=ttyS0,115200n8 usbcore.old_scheme_first=1 tegraboot=sdmmc tegrapart=recovery:35e00:2800:800,linux:34700:1000:800,mbr:400:200:800,system:600:2bc00:800,cache:2c200:8000:800,misc:34200:400:800,userdata:38700:c0000:800 androidboot.hardware=p990"
+	cline="mem=447M@0M nvmem=64M@448M loglevel=0 muic_state=1 lpj=9994240 CRC=3010002a8e458d7 vmalloc=256M brdrev=1.0 video=tegrafb console=ttyS0,115200n8 usbcore.old_scheme_first=1 tegraboot=sdmmc tegrapart=recovery:35e00:2800:800,linux:34700:1000:800,mbr:400:200:800,system:600:2bc00:800,cache:2c200:8000:800,misc:34200:400:800,userdata:38700:c0000:800 androidboot.hardware=p990"
 fi
 ui_print "Selected correct Kernel version..."
 ui_print ""
