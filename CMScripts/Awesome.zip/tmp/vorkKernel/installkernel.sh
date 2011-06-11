20740a348167a663af9c1a599aa5db51051c78f3
Emil Lauridsen <mine809@gmail.com>
2011-06-11 17:03:25 +0200

    General script cleanup

--- CMScripts/Awesome.zip/tmp/vorkKernel/installkernel.sh
+++ CMScripts/Awesome.zip/tmp/vorkKernel/installkernel.sh
@@ -11,12 +11,7 @@ fatal() { ui_print "$@"; exit 1; }
 basedir=`dirname $0`
 BB=$basedir/busybox
 awk="$BB awk"
-grep="$BB grep"
 chmod="$BB chmod"
-chown="$BB chown"
-chgrp="$BB chgrp"
-cpio="$BB cpio"
-find="$BB find"
 warning=0
 
 updatename=`echo $UPDATE_FILE | $awk '{ sub(/^.*\//,"",$0); sub(/.zip$/,"",$0); print }'`
@@ -30,54 +25,54 @@ ui_print ""
 ui_print "Parsing parameters..."
 flags=
 for pp in $args; do
-  case $pp in
-  "1080p")
-        hdrec=1
-        flags="$flags -1080p"
-  ;;
-  "bc")
-        baconcooker=1
-        flags="$flags -baconcooker"
-  ;;
-  "lecam")
-        if [ "$hdrec" == "1" ]; then
-          lecam=1
-          flags="$flags -leCam"
-          ui_print "Thanks to LeJay for his cam mod"
-        else
-          fatal "ERROR: 1080p needs to be enabled for this!"
-        fi
-  ;;
-  "405")
-        if [ "$ril502" == "1" ]; then
-            fatal "ERROR: Only one RIL can be flashed!"
-        fi
-        ril405=1
-        flags="$flags -405"
-  ;;
-  "502")
-        if [ "$ril405" == "1" ]; then
-            fatal "ERROR: Only one RIL can be flashed!"
-        fi
-        ril502=1
-        flags="$flags -502"
-  ;;
-  "silent")
-        silent=1
-        flags="$flags -silent"
-  ;;
-  "boost")
-	boost=1
-	flags="$flags -boost"
-  ;;
-  "density")
-	density=1
-	flags="$flags -density"
-  ;;
-  *)
-        fatal "ERROR: Unknown argument -$pp"
-  ;;
-  esac
+    case $pp in
+        "1080p")
+            hdrec=1
+            flags="$flags -1080p"
+        ;;
+        "bc")
+            baconcooker=1
+            flags="$flags -baconcooker"
+        ;;
+        "lecam")
+            if [ "$hdrec" == "1" ]; then
+                lecam=1
+                flags="$flags -leCam"
+                ui_print "Thanks to LeJay for his cam mod"
+            else
+                fatal "ERROR: 1080p needs to be enabled for this!"
+            fi
+        ;;
+        "405")
+            if [ "$ril502" == "1" ]; then
+                fatal "ERROR: Only one RIL can be flashed!"
+            fi
+            ril405=1
+            flags="$flags -405"
+        ;;
+        "502")
+            if [ "$ril405" == "1" ]; then
+                fatal "ERROR: Only one RIL can be flashed!"
+            fi
+            ril502=1
+            flags="$flags -502"
+        ;;
+        "silent")
+            silent=1
+            flags="$flags -silent"
+        ;;
+        "boost")
+            boost=1
+            flags="$flags -boost"
+        ;;
+        "density")
+            density=1
+            flags="$flags -density"
+        ;;
+        *)
+            fatal "ERROR: Unknown argument -$pp"
+        ;;
+    esac
 done
 
 ui_print "Parsing complete"
@@ -94,20 +89,21 @@ cd $basedir
 
 #Choose Kernel
 if [ "$hdrec" == "1" ]; then
-	if [ "$baconcooker" == "1" ]; then 
-		cp $basedir/Images/1080p/zImageBC $basedir/zImage
-	else
-		cp $basedir/Images/1080p/zImage $basedir/zImage
-	fi
-	cline="mem=383M@0M nvmem=128M@384M loglevel=0 muic_state=1 lpj=9994240 CRC=3010002a8e458d7 vmalloc=256M brdrev=1.0 video=tegrafb console=ttyS0,115200n8 usbcore.old_scheme_first=1 tegraboot=sdmmc tegrapart=recovery:35e00:2800:800,linux:34700:1000:800,mbr:400:200:800,system:600:2bc00:800,cache:2c200:8000:800,misc:34200:400:800,userdata:38700:c0000:800 androidboot.hardware=p990"
+    if [ "$baconcooker" == "1" ]; then 
+        cp $basedir/Images/1080p/zImageBC $basedir/zImage
+    else
+        cp $basedir/Images/1080p/zImage $basedir/zImage
+    fi
+    cline="mem=383M@0M nvmem=128M@384M loglevel=0 muic_state=1 lpj=9994240 CRC=3010002a8e458d7 vmalloc=256M brdrev=1.0 video=tegrafb console=ttyS0,115200n8 usbcore.old_scheme_first=1 tegraboot=sdmmc tegrapart=recovery:35e00:2800:800,linux:34700:1000:800,mbr:400:200:800,system:600:2bc00:800,cache:2c200:8000:800,misc:34200:400:800,userdata:38700:c0000:800 androidboot.hardware=p990"
 else
-	if [ "$baconcooker" == "1" ]; then 
-		cp $basedir/Images/zImageBC $basedir/zImage
-	else
-		cp $basedir/Images/zImage $basedir/zImage
-	fi
-	cline="mem=447M@0M nvmem=64M@448M loglevel=0 muic_state=1 lpj=9994240 CRC=3010002a8e458d7 vmalloc=256M brdrev=1.0 video=tegrafb console=ttyS0,115200n8 usbcore.old_scheme_first=1 tegraboot=sdmmc tegrapart=recovery:35e00:2800:800,linux:34700:1000:800,mbr:400:200:800,system:600:2bc00:800,cache:2c200:8000:800,misc:34200:400:800,userdata:38700:c0000:800 androidboot.hardware=p990"
+    if [ "$baconcooker" == "1" ]; then 
+        cp $basedir/Images/zImageBC $basedir/zImage
+    else
+        cp $basedir/Images/zImage $basedir/zImage
+    fi
+    cline="mem=447M@0M nvmem=64M@448M loglevel=0 muic_state=1 lpj=9994240 CRC=3010002a8e458d7 vmalloc=256M brdrev=1.0 video=tegrafb console=ttyS0,115200n8 usbcore.old_scheme_first=1 tegraboot=sdmmc tegrapart=recovery:35e00:2800:800,linux:34700:1000:800,mbr:400:200:800,system:600:2bc00:800,cache:2c200:8000:800,misc:34200:400:800,userdata:38700:c0000:800 androidboot.hardware=p990"
 fi
+
 ui_print "Selected correct Kernel version..."
 ui_print ""
 ui_print "Building boot.img..."
@@ -115,6 +111,7 @@ $basedir/mkbootimg --kernel $basedir/zImage --ramdisk $basedir/ramdisk-boot --cm
 if [ "$?" -ne 0 -o ! -f boot.img ]; then
     fatal "ERROR: Packing kernel failed!"
 fi
+
 ui_print ""
 ui_print "Flashing the kernel..."
 $BB dd if=/dev/zero of=/dev/block/mmcblk0p5
@@ -122,62 +119,67 @@ $BB dd if=$basedir/boot.img of=/dev/block/mmcblk0p5
 if [ "$?" -ne 0 ]; then
     fatal "ERROR: Flashing kernel failed!"
 fi
+
 ui_print ""
 ui_print "Installing kernel modules..."
 rm -rf /system/lib/modules
 cp -r files/lib/modules /system/lib/
 if [ "$?" -ne 0 -o ! -d /system/lib/modules ]; then
-        ui_print "WARNING: kernel modules not installed!"
-        warning=$((warning + 1))
+    ui_print "WARNING: kernel modules not installed!"
+    warning=$((warning + 1))
 fi
+
 ui_print ""
 if [ -n "$flags" ]; then
-ui_print "Installing additional mods..."
+    ui_print "Installing additional mods..."
 fi
+
 # LeCam
 if [ "$lecam" == "1" ]; then
-  cp $basedir/files/Camera.apk /system/app/Camera.apk
-  $chmod 644 /system/app/Camera.apk
+    cp $basedir/files/Camera.apk /system/app/Camera.apk
+    $chmod 644 /system/app/Camera.apk
 fi
+
 # silent cam
 if [ "$silent" == "1" ]; then
-  mv /system/media/audio/ui/camera_click.ogg /system/media/audio/ui/camera_click.ogg.bak
-  mv /system/media/audio/ui/VideoRecord.ogg /system/media/audio/ui/VideoRecord.ogg.bak
+    mv /system/media/audio/ui/camera_click.ogg /system/media/audio/ui/camera_click.ogg.bak
+    mv /system/media/audio/ui/VideoRecord.ogg /system/media/audio/ui/VideoRecord.ogg.bak
 fi
+
 # Media Profiles
 if [ "$hdrec" == "1" ]; then
-	rm /system/etc/media_profiles.xml
-	if [ "$lecam" == "1" ]; then
-	  cp $basedir/files/media_profiles.xml-le1080 /system/etc/media_profiles.xml
-	else
-          cp $basedir/files/media_profiles.xml-1080 /system/etc/media_profiles.xml
-        fi
+    rm /system/etc/media_profiles.xml
+    if [ "$lecam" == "1" ]; then
+        cp $basedir/files/media_profiles.xml-le1080 /system/etc/media_profiles.xml
+    else
+        cp $basedir/files/media_profiles.xml-1080 /system/etc/media_profiles.xml
+    fi
 else
-	rm /system/etc/media_profiles.xml
-    	cp $basedir/files/media_profiles.xml-720 /system/etc/media_profiles.xml
+    rm /system/etc/media_profiles.xml
+    cp $basedir/files/media_profiles.xml-720 /system/etc/media_profiles.xml
 fi
 
 # Ril 405
 if [ "$ril405" == "1" ]; then
-	rm /system/lib/lge-ril.so
-	cp $basedir/files/ril/405/lge-ril.so /system/lib/lge-ril.so
+    rm /system/lib/lge-ril.so
+    cp $basedir/files/ril/405/lge-ril.so /system/lib/lge-ril.so
 fi
 
 # Ril 502
 if [ "$ril502" == "1" ]; then
-	rm /system/lib/lge-ril.so
-	cp $basedir/files/ril/502/lge-ril.so /system/lib/lge-ril.so
+    rm /system/lib/lge-ril.so
+    cp $basedir/files/ril/502/lge-ril.so /system/lib/lge-ril.so
 fi
 
 #boost
 if [ "$boost" == "1" ]; then
-	cp $basedir/files/80boost /system/etc/init.d/80boost
-	$chmod 750 /system/etc/init.d/80boost
+    cp $basedir/files/80boost /system/etc/init.d/80boost
+    $chmod 750 /system/etc/init.d/80boost
 fi
 
 #density
 if [ "$density" == "1" ]; then
-	sed -n "s/lcd_density=240/lcd_density=220/" /system/build.prop
+    sed -n "s/lcd_density=240/lcd_density=220/" /system/build.prop
 fi
 
 ui_print ""
