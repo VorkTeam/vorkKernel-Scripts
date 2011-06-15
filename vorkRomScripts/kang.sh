e79ca9f491f56ed9635a300ece165302b3c5e558
Benedikt Boss <vork.android@gmail.com>
2011-06-15 11:34:23 -0400

    Add a rom build script

---
+++ vorkRomScripts/kang.sh
@@ -0,0 +1,35 @@
+#!bin/bash
+
+export CM_DIR=$HOME/system/cyanogenmod
+
+clear
+OLDDIR=$PWD
+
+if [ ! -n "$1" ]; then
+	echo "1. make clean"
+	echo "2. make clobber"
+	echo "3. Build ROM"
+	echo -n "Select Option: "
+	read option
+else
+	option=$1
+fi
+
+case  $option in
+  1)
+	cd $CM_DIR
+	make clean
+	make installclean
+	;;
+  2)
+	cd $CM_DIR
+	make clobber
+	;;
+  3)
+	cd $CM_DIR
+	. build/envsetup.sh && brunch p990
+	;;
+  *)
+	echo "Error: not defined!"
+esac
+cd $OLDDIR
