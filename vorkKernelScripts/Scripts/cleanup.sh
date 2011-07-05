5e326ae93f21ec79cf80f4a3afc4fe7767ba1b87
Benedikt Boss <boss.benedikt@googlemail.com>
2011-07-05 01:41:30 -0700

    No need to create a Images folder anymore

--- vorkKernelScripts/Scripts/cleanup.sh
+++ vorkKernelScripts/Scripts/cleanup.sh
@@ -3,11 +3,4 @@
 rm -rf $SCRIPT_DIR/CMScripts/Awesome.zip/system/lib/modules/*
 rm $VORKSCRIPT_DIR/Tools/ramdisk-boot
 rm $VORKSCRIPT_DIR/Tools/newBoot.img
-rm $VORKSCRIPT_DIR/Awesome.zip/tmp/vorkKernel/Images/zImage*
-rm $VORKSCRIPT_DIR/Awesome.zip/tmp/vorkKernel/Images/1080p/zImage*
-if [ ! -d $VORKSCRIPT_DIR/Awesome.zip/tmp/vorkKernel/Images ];then
-mkdir $VORKSCRIPT_DIR/Awesome.zip/tmp/vorkKernel/Images
-fi
-if [ ! -d $VORKSCRIPT_DIR/Awesome.zip/tmp/vorkKernel/Images/1080p ];then
-mkdir $VORKSCRIPT_DIR/Awesome.zip/tmp/vorkKernel/Images/1080p
-fi
+rm $VORKSCRIPT_DIR/Awesome.zip/tmp/vorkKernel/zImage
