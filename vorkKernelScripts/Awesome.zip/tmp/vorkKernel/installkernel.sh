2b74d541d91717dd853286b78e799756c7392313
Emil Lauridsen <mine809@gmail.com>
2011-08-02 10:03:23 +0200

    ROM CHECK, Y U NO WORK?

--- vorkKernelScripts/Awesome.zip/tmp/vorkKernel/installkernel.sh
+++ vorkKernelScripts/Awesome.zip/tmp/vorkKernel/installkernel.sh
@@ -38,16 +38,16 @@ ui_print "Installing $kernelver"
 ui_print "Developed by Benee and kiljacken"
 ui_print ""
 ui_print "Checking ROM..."
-$BB mount /system
-cymo=`cat /system/build.prop | $awk 'tolower($0) ~ /cyanogenmod/ { print "1"; exit 0 }'`
-miui=`cat /system/build.prop | $awk 'tolower($0) ~ /miui/ { print "1"; exit 0 }'`
-if [ "$cymo" == "1" ]; then
-    log "Installing on CyanogenMod"
-elif [ "$miui" == "1" ];
-    log "Installing on Miui"
-else
-    fatal "Current ROM is not compatible with vorkKernel! Aborting..."
-fi
+#$BB mount /system
+#cymo=`cat /system/build.prop | $awk 'tolower($0) ~ /cyanogenmod/ { print "1"; exit 0 }'`
+#miui=`cat /system/build.prop | $awk 'tolower($0) ~ /miui/ { print "1"; exit 0 }'`
+#if [ "$cymo" == "1" ]; then
+#    log "Installing on CyanogenMod"
+#elif [ "$miui" == "1" ];
+#    log "Installing on Miui"
+#else
+#    fatal "Current ROM is not compatible with vorkKernel! Aborting..."
+#fi
 
 ui_print ""
 ui_print "Parsing parameters..."
