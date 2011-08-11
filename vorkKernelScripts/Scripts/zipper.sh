6b7f25076a4bbaac9c42fac4c6e2cf4fc4bd8185
Benedikt Boss <vork.android@gmail.com>
2011-08-11 09:33:45 -0400

    Ugly compile fix script and remove all c comments from the end script

--- vorkKernelScripts/Scripts/zipper.sh
+++ vorkKernelScripts/Scripts/zipper.sh
@@ -3,8 +3,9 @@
 cd $VORKSCRIPT_DIR
 
 if [ "$1" == "Awesome" ]; then
-	cpp -D DEVICE_$device mdfiles/installkernel.pre.sh > $1.zip/tmp/vorkKernel/installkernel.sh
-
+	cpp -D DEVICE_$device mdfiles/installkernel.pre.sh > mdfiles/installkernel.pre2.sh
+wk '/# / { next; } { print; }' mdfiles/installkernel.pre2.sh > mdfiles/installkernel.pre3.sh
+awk 'NR==1{ print "#!/sbin/sh" } { print; }' mdfiles/installkernel.pre3.sh > $1,zip/tmp/vorkKernel/installkernel.sh
 fi
 
 
--- vorkKernelScripts/mdfiles/installkernel.pre.sh
+++ vorkKernelScripts/mdfiles/installkernel.pre.sh
@@ -1,6 +1,3 @@
-#define SHELL_INTRO #!/sbin/sh
-SHELL_INTRO
-
 #ifdef DEVICE_LGP990
 
 #define BOOT_PARTITION 		/dev/block/mmcblk0p5
