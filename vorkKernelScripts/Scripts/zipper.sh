c8b550ac594490d639c8e1256d97423c4dad2d34
Emil Lauridsen <mine809@gmail.com>
2011-08-13 19:55:57 +0200

    Benee, you are such a douche xD

--- vorkKernelScripts/Scripts/zipper.sh
+++ vorkKernelScripts/Scripts/zipper.sh
@@ -6,7 +6,7 @@ if [ "$1" == "Awesome" ]; then
 	echo "#!/sbin/sh" > $1.zip/tmp/vorkKernel/installkernel.sh
 	cpp -D DEVICE_$device mdfiles/installkernel.pre.sh | awk '/# / { next; } { print; }' >> $1.zip/tmp/vorkKernel/installkernel.sh
 	if [ "$device" == "LGP990" ]; then
-		cp -r /mdfiles/ril $1.zip/tmp/vorkKernel/files
+		cp -r mdfiles/ril $1.zip/tmp/vorkKernel/files
 	fi
 fi
 
