ce8b1745752df2547b275b0c957944becd42741b
Benedikt Boss <boss.benedikt@googlemail.com>
2011-09-06 22:23:44 +0300

    Update ccache too

--- vorkChainScripts/ccache.sh
+++ vorkChainScripts/ccache.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 export buildprefix=$HOME/vorkChain
-ccachev=3.1.4
+ccachev=3.1.6
 
 if [ ! -d $buildprefix/source/ccache-$ccachev ]; then
 	echo Downloading ccache...
@@ -13,8 +13,8 @@ fi
 cd $buildprefix/source/ccache-$ccachev
 echo Configuring ccache...
 ./configure
-make -j8
-sudo make install -j8
+make -j`grep "processor" /proc/cpuinfo | wc -l`
+sudo make install -j`grep "processor" /proc/cpuinfo | wc -l`
 
 echo > $HOME/vorkKernel-Scripts/vorkKernelScripts/Tools/ccache.txt
 cd $HOME/vorkKernel-Scripts/ 
